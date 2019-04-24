569b57d613d14d1f4daf8cf7c419682e4974c731
Cauê Santos <cauedossantossilva@hotmail.com>
2019-04-24 19:43:41 -0300

    commit

--- TropicalSistema/Form1.Designer.cs
+++ TropicalSistema/Form1.Designer.cs
@@ -23,10 +23,20 @@
         /// o conteúdo deste método com o editor de código.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1067, 520);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tropical Gourmet";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
