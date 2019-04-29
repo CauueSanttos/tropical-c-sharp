ee7b9ec4a0bca16a60e45cc8919aa31af232d7b3
Cauê Santos <cauedossantossilva@hotmail.com>
2019-04-29 19:45:29 -0300

    Continuação do Projeto

---
+++ TropicalSistema/include/model/Cliente.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TropicalSistema.include.model {
+
+    /**
+     * Modelo de dados de Cliente
+     * @author Cauê dos Santos Silva
+     */
+    class Cliente {
+
+        private int codigo;
+        private string nome;
+        private string telefone;
+        private string empresa;
+        private int tipo;
+
+        public void setCodigo(int iCodigo) {
+            this.codigo = iCodigo;
+        }
+
+        public int getCodigo() {
+            return this.codigo;
+        }
+
+        public void setNome(string sNome) {
+            this.nome = sNome;
+        }
+
+        public string getNome() {
+            return this.nome;
+        }
+
+        public void setTelefone(string sTelefone) {
+            this.telefone = sTelefone;
+        }
+
+        public string getTelefone() {
+            return this.telefone;
+        }
+
+        public void setEmpresa(string sEmpresa) {
+            this.empresa = sEmpresa;
+        }
+
+        public string getEmpresa() {
+            return this.empresa;
+        }
+
+        public void setTipo(int iTipo) {
+            this.tipo = iTipo;
+        }
+
+        public int getTipo() {
+            return this.tipo;
+        }
+
+    }
+}
